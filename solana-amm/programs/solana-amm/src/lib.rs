6a1f8be44f9b022c5d66688aa1084e6b654ddd0f
Pawan2061 <pandeypawan2061@gmail.com>
2025-09-10 22:55:01 +0530

    feat: basic normal layout

--- solana-amm/programs/solana-amm/src/lib.rs
+++ solana-amm/programs/solana-amm/src/lib.rs
@@ -6,8 +6,27 @@ declare_id!("BvAYVdAqjoiS9zRawpc6dDktotk1NbRiT7QETT2xZVk9");
 pub mod solana_amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    //     msg!("Greetings from: {:?}", ctx.program_id);
+    //     Ok(())
+    // }
+
+    pub fn initialize_pool(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Grettings");
+        Ok(())
+    }
+
+    pub fn add_liquidity_tokens(ctx: Context<Initialize>) -> Result<()> {
+        msg!("working on adding liquidity tokens");
+        Ok(())
+    }
+    pub fn withdraw(ctx: Context<Initialize>) -> Result<()> {
+        msg!("working on adding liquidity tokens");
+        Ok(())
+    }
+
+    pub fn swap(ctx: Context<Initialize>) -> Result<()> {
+        msg!("swapping tokens");
         Ok(())
     }
 }
