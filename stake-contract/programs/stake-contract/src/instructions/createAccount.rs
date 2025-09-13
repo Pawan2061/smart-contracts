9e557b7f6b67d315c1c2124836e96ae673bec0fe
Pawan2061 <pandeypawan2061@gmail.com>
2025-09-14 03:27:02 +0530

    feat: create stake account

--- stake-contract/programs/stake-contract/src/instructions/createAccount.rs
+++ stake-contract/programs/stake-contract/src/instructions/createAccount.rs
@@ -1,12 +1,36 @@
+use crate::state::StakeAccount;
 use anchor_lang::prelude::*;
 
 pub fn create_stake_account(ctx: Context<CreateAccount>) -> Result<()> {
-    msg!("wokring");
+    let stake_account = &mut ctx.accounts.stake_account;
+
+    stake_account.owner = ctx.accounts.user.key();
+    stake_account.staked_amount = 0;
+    stake_account.total_points = 0;
+    stake_account.stake_timestamp = 0;
+
+    stake_account.bump = ctx.bumps.stake_account;
+
+    msg!(
+        "Stake account created successfully for user: {}",
+        stake_account.owner
+    );
     Ok(())
 }
 
 #[derive(Accounts)]
-
 pub struct CreateAccount<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 32 + 8 + 8 + 8 + 1,
+        seeds = [b"stake_account", user.key().as_ref()],
+        bump
+    )]
+    pub stake_account: Account<'info, StakeAccount>,
+
     pub system_program: Program<'info, System>,
 }
--- stake-contract/programs/stake-contract/src/state/stakeAccount.rs
+++ stake-contract/programs/stake-contract/src/state/stakeAccount.rs
@@ -1,4 +1,10 @@
 use anchor_lang::prelude::*;
 
 #[account]
-pub struct StakeAccount {}
+pub struct StakeAccount {
+    pub owner: Pubkey,
+    pub staked_amount: u64,
+    pub total_points: u64,
+    pub stake_timestamp: i64,
+    pub bump: u8,
+}
